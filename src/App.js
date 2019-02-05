3b46c3f3bdc8e3d00f2eb92cdeadb9c38436f314
amrendra kumar <amarmca51@gmail.com>
2019-02-05 13:13:13 +0530

    this is first commit of react2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'; 
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Amrendra', age: 28},
+      { name : 'sahmila' , age: 22},
+      { name: 'Aditi' , age: 12}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+   //Donot this work this.state.persons[0].name='Kumar';
+   this.setState({
+   persons: [
+    {name: newName, age: 32},
+    { name : 'sahmila' , age: 22},
+    { name: 'Aditi' , age: 12}
+  ]
+  })
+  }
+
+  nameChangeHndler = (event) => {
+    this.setState({
+      persons: [
+       {name: 'textName', age: 32},
+       { name : event.target.value , age: 22},
+       { name: 'Aditi' , age: 12}
+     ]
+     })
+  }
+
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> Hi, I'am a React application </h1> 
+        <button 
+          style={style}
+        onClick={() => this.switchNameHandler('amrendra..') }>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} 
+        click={this.switchNameHandler.bind(this, 'Amrendra!!!')}
+        changed={this.nameChangeHndler}> 
+        My Hobbies is: Racing </Person>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} />
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} />
+        
       </div>
+     
     );
   }
 }
